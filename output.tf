217bb86fdc2da57522218338a594d229e17f940c
Kritika Das <kritika.das_ex@kdrp.com>
2026-07-01 09:03:58 -0500

    updated code

--- output.tf
+++ output.tf
@@ -14,9 +14,6 @@ output "firewall_name"{
     value = google_compute_firewall.allow_ssh_http.name
 }
 
-output "vm_name"{
-    description = "virtual machine name"
-    value = google_compute_instance.vm.name
-}
+
 
 
